d1e491126d34d85fd15a5f92e457800b2e4c4a01
Oleg Voitiuk <oleg.voitiuk.natife@gmail.com>
2023-09-07 11:11:16 +0300

    init: new project

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", function (req, res) {
+    res.json({ message: "Hello World!" });
+});
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
